d40e1ceedad7959866330f05a522e44519fae544
Gautam Shah <shahga@users.noreply.github.com>
2019-03-05 23:46:38 -0500

    Initial Setup

---
+++ tools-setup.ps1
@@ -0,0 +1,53 @@
+#=============================================================================
+# ! TOOLS INSTALLATION ON WINDOWS DESKTOP
+#=============================================================================
+
+
+#=============================================================================
+# ! VSCODE - INSTALL
+
+https://code.visualstudio.com/Docs/setup/setup-overview
+
+# * VSCODE EXTENSIONS
+
+# POWERSHELL
+# C#
+# DOCKER
+# KUBERNETES
+# YAML
+# BETTER COMMENTS
+#=============================================================================
+
+
+#=============================================================================
+# ! AZURE CLI - INSTALL
+
+https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
+#=============================================================================
+
+
+#=============================================================================
+# ! DOCKER INSTALL
+
+https://hub.docker.com/editions/community/docker-ce-desktop-windows
+#=============================================================================
+
+
+#=============================================================================
+# ! KUBERNETES - KUBECTL INSTALL
+
+# INSTALL KUBERNETES FROM SITE
+https://kubernetes.io/docs/tasks/tools/install-kubectl/
+
+# OR VIA AZURE CLI
+az aks install-cli
+
+# AZ CLI ENABLE PREVIEW FEATURES E.G. CLUSTER AUTO SCALER
+az extension add --name aks-preview
+
+# AZ CLI EXTENSION > AKS VIRTUAL NODE
+az extension add --source https://aksvnodeextension.blob.core.windows.net/aks-virtual-node/aks_virtual_node-0.2.0-py2.py3-none-any.whl
+
+# HELM INSTALL
+https://github.com/helm/helm
+#=============================================================================
